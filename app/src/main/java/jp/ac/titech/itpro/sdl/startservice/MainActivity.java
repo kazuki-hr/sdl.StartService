8f9b49c512587238464e3c1c6eca8bd36e0b29d2
Kazuki Hirohata <hirohata.k.aa@m.titech.ac.jp>
2021-05-25 17:08:18 +0900

    add Service3

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,12 +12,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
+    public final static String EXTRA_ANSWER = "ANSWER";
+    public final static String ACTION_ANSWER = "jp.ac.titech.itpro.sdl.startservice.answer";
+    private BroadcastReceiver scanReceiver;
+    private IntentFilter scanFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+        scanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Log.d(TAG, "onReceive: " + action);
+                if (action == null) {
+                    return;
+                }
+                if (ACTION_ANSWER.equals(action)) {
+                    Log.d(TAG, "myarg = " + intent.getStringExtra(EXTRA_ANSWER));
+                }
+            }
+        };
+        scanFilter = new IntentFilter();
+        scanFilter.addAction(ACTION_ANSWER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(scanReceiver, scanFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(scanReceiver);
     }
 
     public void onClickTest1(View v) {
@@ -28,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onClickTest2 in " + Thread.currentThread());
         Intent intent = new Intent(this, Service2.class);
         intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+
+    public void onClickTest3(View v) {
+        Log.d(TAG, "onClickTest3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, Service3.class);
         startService(intent);
     }
 }
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/Service3.java
@@ -0,0 +1,40 @@
+package jp.ac.titech.itpro.sdl.startservice;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class Service3 extends IntentService {
+    private final static String TAG = Service3.class.getSimpleName();
+
+    public Service3() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent in " + Thread.currentThread());
+        Intent intent1 = new Intent();
+        intent1.setAction(MainActivity.ACTION_ANSWER);
+        intent1.putExtra(MainActivity.EXTRA_ANSWER, "Hello, MainActivity");
+        sendBroadcast(intent1);
+        try {
+            Thread.sleep(5000); // 5 sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate in " + Thread.currentThread());
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy in " + Thread.currentThread());
+        super.onDestroy();
+    }
+}
+
